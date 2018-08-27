07c4f1bd4201d8707ac5fdf6e93c5a5651ea2653
L Daniel Swakman <d.swakman@gmail.com>
2018-08-27 12:10:27 +0200

    design mockup update

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,83 @@
+// Component: smoothScroll
+$(document).ready(function() {
+  $('a[href^="#"]').smoothScroll({
+    speed: 1000,
+    easing: 'easeInOutQuart',
+    afterScroll: function() {
+      updateHash($(this).attr('href'));
+    }
+  });
+});
+// Update URL hash
+function updateHash(href) {
+  // Restore original URL + search query if href is ''
+  if(href.length < 1) {
+    href = window.location.origin + window.location.pathname + window.location.search;
+  }
+  if(history.pushState) {
+    history.pushState(null, null, href);
+  }
+  else {
+    location.hash = href;
+  }
+}
+
+
+
+
+
+// Component: menuBehaviour
+scrollevents = 'ready scroll resize scrollstart scrollstop';
+$(document).on(scrollevents, function() {
+  if($('.menu').offset().top > $(window).height()) {
+  	$('.menu').addClass('isStuck');
+  } else {
+  	$('.menu').removeClass('isStuck');
+  }
+});
+
+
+
+
+
+// Component: activeSections
+scrollevents = 'ready scroll resize scrollstart scrollstop';
+$(document).on(scrollevents, function() {
+  navOnScroll($('section'), $('.menu'));
+});
+
+function navOnScroll($target, $nav) {
+  scroll = $(window).scrollTop();
+  delta = 100000; // big number
+  active_id = null;
+
+  $target.each(function() {
+    scrollpoint = scroll + $(window).outerHeight()/2;
+    midpoint = $(this).offset().top + $(this).outerHeight()/2;
+    if (Math.abs(scrollpoint - midpoint) < delta) {
+      delta = Math.abs(scrollpoint - midpoint);
+      active_id = ($(this).attr('id').length > 0) ? $(this).attr('id') : null;
+    }
+  });
+
+  if(active_id && active_id.length > 0 && $target.filter('.isActive').attr('id') !== active_id) {
+    // add active class to slide
+    $target.removeClass('isActive');
+    $target.filter('[id="' + active_id + '"]').addClass('isActive');
+    // add active class to nav
+    $nav.find('a').removeClass('isActive');
+    $nav.find('li').removeClass('isActive');
+    $nav.find('a[href="#' + active_id + '"]').addClass('isActive');
+    $nav.find('a[href="#' + active_id + '"]').closest('li').addClass('isActive');
+  }
+}
+
+
+
+
+
+
+// Component: Sine Curves on Canvas
 var xspacing = 1;   // Distance between each horizontal location
 var w;              // Width of entire wave
 var maxwaves = 5;   // total # of waves to add together
@@ -12,7 +92,8 @@ var dx = new Array(maxwaves);
 var yvalues;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	var canvas = createCanvas(windowWidth, windowHeight);
+	canvas.parent('canvas');
 	frameRate(30);
 	colorMode(RGB, 17, 22, 30, 255);
 	w = width + 10;
